069b48788f5a22f2d20c578f5465fe14e659ce49
Gabriel <caephler@gmail.com>
2021-08-17 14:10:47 +0800

    change script lang

--- src/lib/GameWinLosePopup.svelte
+++ src/lib/GameWinLosePopup.svelte
@@ -1,4 +1,4 @@
-<script lang="typescript">
+<script lang="ts">
   import { cubicOut, elasticOut } from "svelte/easing";
   export let winState: "win" | "lose" | "progress";
   export let reset: () => void;
